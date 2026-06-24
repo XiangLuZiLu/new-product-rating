606f65896d7799d4133bdcb34b186e22f19969c4
XiangLuZiLu <3549580957@qq.com>
2026-06-24 15:44:29 +0800

    Delete functions directory

--- functions/api/logout.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestPost() {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: {
-      'content-type': 'application/json; charset=utf-8',
-      'Set-Cookie': 'session=; HttpOnly; SameSite=Lax; Path=/; Max-Age=0'
-    }
-  });
-}
